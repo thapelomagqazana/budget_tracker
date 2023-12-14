ed8cc20e27bbe5e38579665d1a2383604d15d023
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-12-14 14:03:35 +0200

    Added webpack

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// app.js
+
+// Import necessary modules or files
+import { getTransactionsFromLocalStorage, saveTransactionsToLocalStorage, addTransaction, deleteTransaction } from './transactions';
+
+// Function to initialize the application
+function initializeApp() {
+  // Add your initialization logic here
+  console.log('Initializing the application');
+
+  // Example: Load transactions from localStorage
+  const transactions = getTransactionsFromLocalStorage();
+  console.log('Loaded transactions:', transactions);
+
+  // Example: Add a new transaction
+  addTransaction('Salary', 2000, 'income');
+  console.log('Added a new transaction:', getTransactionsFromLocalStorage());
+
+  // Example: Delete a transaction
+  deleteTransaction(0);
+  console.log('Deleted a transaction:', getTransactionsFromLocalStorage());
+}
+
+// Initialize the application when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', initializeApp);
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+};
+
